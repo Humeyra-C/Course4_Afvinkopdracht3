83dadb020ab92e38bc0a1074c91da694aa55cab8
Humeyra-C <h.copoglu@student.han.nl>
2022-05-13 16:42:20 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+"""
+Author: Humeyra Copoglu
+Date: 25-04-2022
+Content: BIN-1a OWE4 Afvinkopdracht 3B
+"""
+from flask import Flask
+import mysql_connect as db
+import mysql.connector as sql
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    connect = db
+    if connect.success:
+        return 'Hello World!'
+    else:
+        return 'Connection failed!'
+
+
+if __name__ == '__main__':
+    app.run()
+
+
---
+++ mysql_connect.py
@@ -0,0 +1,21 @@
+"""
+Author: Humeyra Copoglu
+Date: 25-04-2022
+Content: BIN-1a OWE4 Afvinkopdracht 3B
+"""
+import mysql.connector as sql
+success = False
+
+try:
+    sql.connect(host='145.74.104.145',
+                user='ebcra',
+                password='Aa644370!',
+                db='blok3')
+except Exception as e:
+    success = False
+else:
+    success = True
+
+# sql.connect(host='ensembldb.ensembl.org',
+#             user='anonymous',
+#             db='homo_sapiens_core_95_38')
